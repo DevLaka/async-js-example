dc128aec0525f95db6153b5d078dd039f9a5783c
devlaka <lakshanwarunab@gmail.com>
2022-07-04 17:08:16 +0530

    add fake api with callbacks

--- main.js
+++ main.js
@@ -29,3 +29,68 @@ changeColor("#FFC54D", 1000, () => {
     });
   });
 });
+
+// Step 4: Another exmaple: Calling a fake API that use callbacks.
+// Just focus on calling the API. Writing promises will be converd in next steps.
+
+// Fake API
+// success: callback which runs if task is sucessfull.
+// failure callback which runs if task is unscessfull.
+const fakeRequest = (url, success, failure) => {
+  const delay = Math.floor(Math.random() * 5000);
+  setTimeout(() => {
+    // Task fails
+    if (delay > 3000) {
+      // Calls failure callback
+      failure("Connection Timeout.");
+    }
+    // Task successfull
+    else {
+      // Calls success callback
+      success(`Hi. This is your data from ${url}`);
+    }
+  }, delay);
+};
+
+// Calling the API
+fakeRequest(
+  "money.com/page1",
+  // success callback
+  function (response) {
+    console.log("Request sucessful Page1");
+    console.log(response);
+    // If and after request to page 1 sucessfull request for page 2.
+    fakeRequest(
+      "money.com/page2",
+      // success callback
+      function (response) {
+        console.log("Request sucessful Page2");
+        console.log(response);
+        // If and after request to page 2 sucessfull request for page 3.
+        fakeRequest(
+          "money.com/page3",
+          // success callback
+          function (response) {
+            console.log("Request sucessful Page3");
+            console.log(response);
+          },
+          // failure callback
+          function (err) {
+            console.log("Request failed Page3");
+            console.log(err);
+          }
+        );
+      },
+      // failure callback
+      function (err) {
+        console.log("Request failed Page2");
+        console.log(err);
+      }
+    );
+  },
+  // failure callback
+  function (err) {
+    console.log("Request failed Page1");
+    console.log(err);
+  }
+);
