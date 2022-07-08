bf9bc60796054060d4ec0a64b04d3480ac5dfd3c
devlaka <lakshanwarunab@gmail.com>
2022-07-08 18:20:02 +0530

    promise chaining

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Step 1: Chaning background color after 1 second delay.
+// Step 1: Changing background color after 1 second delay.
 // setTimeout(() => {
 //   document.body.style.backgroundColor = "#FFC54D";
 //   setTimeout(() => {
@@ -21,7 +21,7 @@ const changeColor = (color, delay, doAfter) => {
 };
 
 // Step 3: Calling the step 2 function.
-// EXMAPLE OF CALLBACK HELL.
+// EXAMPLE OF CALLBACK HELL.
 changeColor("#FFC54D", 1000, () => {
   changeColor("#BD4291", 1000, () => {
     changeColor("#F94C66", 1000, () => {
@@ -30,12 +30,12 @@ changeColor("#FFC54D", 1000, () => {
   });
 });
 
-// Step 4: Another exmaple: Calling a fake API that use callbacks.
-// Just focus on calling the API. Writing promises will be converd in next steps.
+// Step 4: Another example: Calling a fake API that use callbacks.
+// Just focus on calling the API. Writing promises will be covered in next steps.
 
 // Fake API
-// success: callback which runs if task is sucessfull.
-// failure callback which runs if task is unscessfull.
+// success callback runs if task is successful.
+// failure callback runs if task is unsuccessful.
 const fakeRequest = (url, success, failure) => {
   const delay = Math.floor(Math.random() * 5000);
   setTimeout(() => {
@@ -44,7 +44,7 @@ const fakeRequest = (url, success, failure) => {
       // Calls failure callback
       failure("Connection Timeout.");
     }
-    // Task successfull
+    // Task successful
     else {
       // Calls success callback
       success(`Hi. This is your data from ${url}`);
@@ -57,21 +57,21 @@ const fakeRequest = (url, success, failure) => {
 //   "money.com/page1",
 //   // success callback
 //   function (response) {
-//     console.log("Request sucessful Page1");
+//     console.log("Request successful Page1");
 //     console.log(response);
-//     // If and after request to page 1 sucessfull request for page 2.
+//     // If and after request to page 1 successful request for page 2.
 //     fakeRequest(
 //       "money.com/page2",
 //       // success callback
 //       function (response) {
-//         console.log("Request sucessful Page2");
+//         console.log("Request successful Page2");
 //         console.log(response);
-//         // If and after request to page 2 sucessfull request for page 3.
+//         // If and after request to page 2 successful request for page 3.
 //         fakeRequest(
 //           "money.com/page3",
 //           // success callback
 //           function (response) {
-//             console.log("Request sucessful Page3");
+//             console.log("Request successful Page3");
 //             console.log(response);
 //           },
 //           // failure callback
@@ -96,10 +96,10 @@ const fakeRequest = (url, success, failure) => {
 // );
 
 // Step 5: Calling a fake API that uses Promise.
-// Just focus on calling the API. Writing promises will be converd in next steps.
+// Just focus on calling the API. Writing promises will be covered in next steps.
 
 // Same API in step 4 but using Promise.
-// Only URL as a parameter. Doesn't use callbacks.
+// Only URL as a parameter. No callbacks.
 
 // Promises have 3 states.
 // 1. pending : Waiting for task to complete. Eventually, will be rejected or resolved.
@@ -110,7 +110,7 @@ const fakeRequest2 = (url) => {
     const delay = Math.floor(Math.random() * 5000);
     setTimeout(() => {
       if (delay > 3000) {
-        reject("Connection Timeout");
+        reject(`Connection Timeout. Failed request to ${url}`);
       } else {
         resolve(`Hi. This is your data from ${url}`);
       }
@@ -118,40 +118,67 @@ const fakeRequest2 = (url) => {
   });
 };
 
-const req = fakeRequest2("money.com/page1");
-console.log(req);
+// const req = fakeRequest2("money.com/page1");
+// console.log(req);
+// fakeRequest2 returns an Promise object.
 // req is an Promise object. Contains then and catch methods. We pass callbacks to those 2 methods.
-// then runs when the Promise is resolved.
-// catch runs when the Prmise is rejected.
+// "then" runs when the Promise is resolved.
+// "catch" runs when the Promise is rejected.
 // In another words, the callback we pass to the then will be the function that runs when resolved.
 // In another words, the callback we pass to the catch will be the function that runs when rejected.
-// In another words, We are sending the callback functions to Prmoise. And ask Prmise to execute those functions
+// In another words, We are sending the callback functions to Promise. And ask Promise to execute those functions
 // with his data when he is ready.
-req
+// req
+//   .then((response) => {
+//     console.log("Request successful Page1");
+//     console.log(response);
+//     // Here, for example I don't save the promise to an object like page1 request.
+//     fakeRequest2("money.com/page2")
+//       .then((response) => {
+//         console.log("Request successful Page2");
+//         console.log(response);
+//         fakeRequest2("money.com/page3")
+//           .then((response) => {
+//             console.log("Request successful Page3");
+//             console.log(response);
+//           })
+//           .catch((err) => {
+//             console.log("Request failed Page3");
+//             console.log(err);
+//           });
+//       })
+//       .catch((err) => {
+//         console.log("Request failed Page2");
+//         console.log(err);
+//       });
+//   })
+//   .catch((err) => {
+//     console.log("Request failed Page1");
+//     console.log(err);
+//   });
+
+// Step 6: Make improvements to Step 5.
+// We can write step 5 in better way.
+// Because fakeRequestPromise() returns a Promise we don't have to nest.
+// We can chain the "then" methods on Promise objects returned by calling fakeRequestPromise() method.
+// A single catch method will do the job here.
+fakeRequest2("money.com/page1")
   .then((response) => {
-    console.log("Request sucessfull Page1");
+    console.log("Request successful Page1");
     console.log(response);
-    // Here, for example I don't save the promise to an object like page1 request.
-    fakeRequest2("money.com/page2")
-      .then((response) => {
-        console.log("Request sucessfull Page2");
-        console.log(response);
-        fakeRequest2("money.com/page3")
-          .then((response) => {
-            console.log("Request sucessfull Page3");
-            console.log(response);
-          })
-          .catch((err) => {
-            console.log("Request failed Page3");
-            console.log(err);
-          });
-      })
-      .catch((err) => {
-        console.log("Request failed Page2");
-        console.log(err);
-      });
+    // return a Promise
+    return fakeRequest2("money.com/page2");
+  })
+  .then((response) => {
+    console.log("Request successful Page2");
+    console.log(response);
+    // return a Promise
+    return fakeRequest2("money.com/page3");
+  })
+  .then((response) => {
+    console.log("Request successful Page3");
+    console.log(response);
   })
   .catch((err) => {
-    console.log("Request failed Page1");
     console.log(err);
   });
